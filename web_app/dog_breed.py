1fbf7a009f6aad0e470e98e411951e32f9196bbe
WojciechO <wojciech.orchowski@gmail.com>
2022-03-30 23:04:47 +0200

    Making the app ready for deployment

--- web_app/dog_breed.py
+++ web_app/dog_breed.py
@@ -1,2 +1,4 @@
 from dog_breed_app import app
-app.run(host='0.0.0.0', port=3001, debug=True, threaded=False)
+
+#uncomment the line below to run the app locally 
+# app.run(host='0.0.0.0', port=3001, debug=True, threaded=False)
